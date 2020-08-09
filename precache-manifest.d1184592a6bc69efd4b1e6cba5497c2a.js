e5ab647475267f04db9a64698c4e76b8cad9d822
dkupshm <dkupsh@ucsb.edu>
2020-08-09 14:15:04 -0700

    Updates

---
+++ precache-manifest.d1184592a6bc69efd4b1e6cba5497c2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb86389f17005e53e19990fa87a9e0b",
+    "url": "/stvote/index.html"
+  },
+  {
+    "revision": "36959245fc54ad2629da",
+    "url": "/stvote/static/css/2.cbbbb7c5.chunk.css"
+  },
+  {
+    "revision": "2254fef4ff45f621ea3b",
+    "url": "/stvote/static/css/main.c29a71c2.chunk.css"
+  },
+  {
+    "revision": "36959245fc54ad2629da",
+    "url": "/stvote/static/js/2.ec74e482.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/stvote/static/js/2.ec74e482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2254fef4ff45f621ea3b",
+    "url": "/stvote/static/js/main.7b44c900.chunk.js"
+  },
+  {
+    "revision": "c79a6a54bdbed3633120",
+    "url": "/stvote/static/js/runtime-main.3fe2677a.js"
+  }
+]);
